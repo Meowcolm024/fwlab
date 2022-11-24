85ca188cbcc4b3c89ab89e4227d18ff4fbef2061
Meowcolm024 <luoyicong1026@live.com>
2022-11-24 18:05:16 +0100

    proj

---
+++ projcode/Heap.hs
@@ -0,0 +1,31 @@
+module Heap where
+
+data Heap a = Empty | Heap a [Heap a] deriving Show
+
+merge :: Ord a => Heap a -> Heap a -> Heap a
+merge Empty h     = h
+merge h     Empty = h
+merge h1@(Heap x hs1) h2@(Heap y hs2) | x < y     = Heap x (h2 : hs1)
+                                      | otherwise = Heap y (h1 : hs2)
+
+insHeap :: Ord a => Heap a -> [Heap a] -> Heap a
+insHeap h hs = foldl merge h hs
+
+insert :: Ord a => a -> Heap a -> Heap a
+insert x = merge (Heap x [])
+
+extractMin :: Ord a => Heap a -> Maybe (a, Heap a)
+extractMin Empty             = Nothing
+extractMin (Heap x []      ) = Just (x, Empty)
+extractMin (Heap x (h : hs)) = Just (x, insHeap h hs)
+
+fromList :: Ord a => [a] -> Heap a
+fromList xs = foldr insert Empty xs
+
+toList :: Ord a => Heap a -> [a]
+toList h = case extractMin h of
+    Nothing     -> []
+    Just (h, t) -> h : toList t
+
+heapSort :: Ord a => [a] -> [a]
+heapSort = toList . fromList
